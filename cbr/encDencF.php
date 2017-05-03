ae4fd22289a8caea10442038742b9328d2697f77
AngelLeonardoBianco <angel.leonardo.bianco@outlook.com>
2017-05-03 00:02:01 -0300

    :boom::camel: Added .gitattributes & .gitignore files
    lets playa game

---
+++ cbr/encDencF.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Created by Angel Leonardo Bianco on 01/05/2017
+* for Traftecno
+* angel.leonardo.bianco@gmail.com
+*/
+
+
+//// TODO include encriptation decryt functions here, we neeed three, one for encryptsimple data with openssl, one for decript data with opensll, and last one for encrytop the password
+
+?>
